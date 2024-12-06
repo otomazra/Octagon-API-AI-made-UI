02d73cbd7b4a8ee8921d44b4ef30aa21cdd5bc4e
otomazra <oto.mazra@gmail.com>
2024-12-07 03:20:46 +0400

    Improve css styling for newly added ejs anchor tags

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/rankings", async (req,res)=>{
     const response = await axios.get("https://api.octagon-api.com/rankings");
     const data=(response.data);
-    // console.log((data[2].champion.id));
-    // const result = await axios.get("https://api.octagon-api.com/fighter/"+id)
     res.render("rankings.ejs", {data: data});
 });
 
@@ -33,7 +31,6 @@ app.get("/fighter/:id", async (req,res)=>{
     let id = req.params.id;
     const response = await axios.get("https://api.octagon-api.com/fighter/"+id);
     const data=response.data;
-    // console.log(data.imgUrl);
     res.render("fighter.ejs", {fighter: data});
 });
 
@@ -41,34 +38,14 @@ app.get("/img/:id", async (req,res)=>{
     let id = req.params.id;
     const response = await axios.get("https://api.octagon-api.com/fighter/"+id);
     const data=response.data;
-    // console.log(data.imgUrl);
-    // res.render("fighter.ejs", {fighter: data});
     res.json(data);
 });
 
 app.get("/", async (req,res)=>{
     const response = await axios.get("https://api.octagon-api.com/rankings");
     const data=(response.data);
-    // console.log((data[2].champion.id));
-    // const result = await axios.get("https://api.octagon-api.com/fighter/"+id)
     res.render("index.ejs", {data: data});
-})
-
-// app.get("/divisions", async (req,res)=>{
-//     const response = await axios.get("https://api.octagon-api.com/rankings");
-//     const data=(response.data);
-//     let divisions = [];
-//     const result = await axios.get("https://api.octagon-api.com/fighter/"+id);
-//     data.forEach(division=>{
-//         const div={
-//             id: division.id,
-//             name: division.categoryName,
-//             champion: division.champion.championName
-//         }
-//         divisions.push(div);
-//     })
-//     res.render("divisions.ejs", {divisions: divisions});
-// });
+});
 
 app.listen(port, ()=>{
     console.log("Listening to port: "+port);
